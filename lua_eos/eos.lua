16016f5cefbc4d8679c14f37d72470d125ef74c7
Marcelo Varanda <mv_email@yahoo.com>
2021-02-27 12:42:33 -0500

    %1

--- lua_eos/eos.lua
+++ lua_eos/eos.lua
@@ -1,5 +1,6 @@
 
 -- http://lua-users.org/wiki/LuaStyleGuide
+function show(t) for k,v in pairs(t) do print(k .. " =  ".. v) end end
 
 function _eos_delay(t)
   local x = 0
@@ -12,16 +13,74 @@ function _eos_delay(t)
   end
 end
 
+local function getfenv(fn)
+  local i = 1
+  while true do
+    local name, val = debug.getupvalue(fn, i)
+    if name == "_ENV" then
+      return val
+    elseif not name then
+      break
+    end
+    i = i + 1
+  end
+end
+
+local function setfenv(fn, env)
+  local i = 1
+  while true do
+    local name = debug.getupvalue(fn, i)
+    if name == "_ENV" then
+      debug.upvaluejoin(fn, i, (function()
+        return env
+      end), 1)
+      break
+    elseif not name then
+      break
+    end
+
+    i = i + 1
+  end
+
+  return fn
+end
+
 -- comment out the following line for eos
 eos_delay = _eos_delay
 
 schd = {}
 local tasks = {}
+local user_events = {}
+
+local user_event = 1000
+
+local ST_YIELD = 1
+local ST_WAIT_DELAY = 2
+local ST_WAIT_EVENT = 3
+local ST_ERROR = 4
 
 local function scheduler()
+  local success
+  local args
   while(1) do
     for k,v in pairs(tasks) do
-      success, args = coroutine.resume(v.co)
+      
+      success = true
+      if v.state == ST_YIELD then
+        success, args = coroutine.resume(v.co, v, "ID", "ARGS")
+      elseif v.state ==  ST_WAIT_DELAY then
+        print("WAIT_DELAY not implemeneted")
+        return
+      elseif v.state ==  ST_WAIT_EVENT then
+        if #v.ev_q > 0 then
+          local e = table.remove(v.ev_q, 1)
+          
+        end
+      else
+        print("unexpected e.state = " .. v.state)
+        return
+      end
+      
       if success == false then
         print("Error: " .. args)
         return
@@ -31,16 +90,49 @@ local function scheduler()
   end
 end
 
-local YIELD = 1
-local WAIT_DELAY = 2
-local WAIT_EVENT = 3
+function schd.create_user_event(name, high_priority)
+  local p = false
+  if high_priority ~= nil then
+    p = high_priority
+  end
+  local ev = {}
+  ev.id = user_event
+  ev.name = name
+  ev.pri = high_priority
+  user_event = user_event + 1
+  table.insert(user_events, ev)
+  return ev
+end
 
-function schd.create_task(task_func)
+function schd.subscribe_event(ctx, event)
+  -- chech if event already subscribed
+  for k,v in pairs(ctx.ev_q) do
+    if event.id == v.id then
+      print("event already subscribed")
+      return
+    end
+  end
+  table.insert(ctx.ev_q, event)
+end
+
+function schd.subscribe_event_by_name(ctx, name)
+  for k,v in pairs(user_events) do
+    if name == v.name then
+      schd.subscribe_event(ctx, v)
+      return true
+    end
+  end
+  return false, "could not find event " .. name
+end
+
+
+function schd.create_task(task_func, name)
   local e = {}
   e["co"] = coroutine.create(task_func)
-  e["state"] = YIELD
+  e["name"] = name
+  e["state"] = ST_YIELD
   e["subscription"] = {}
-  e["ev_q"] = {}
+  e["ev_q"] = {} -- entries have "ev_id" and "table_values"
   table.insert(tasks, e)
 end
 
@@ -54,7 +146,11 @@ end
 
 
 
-function task1( t )
+function task1( ctx )
+  print ("starting task " .. ctx.name)
+  
+  local ev = schd.create_user_event("event_1")
+  
   local x = 10
   while(1) do
     print("task1, x = " .. x)
@@ -63,7 +159,14 @@ function task1( t )
   end
 end
 
-function task2( t )
+function task2( ctx )
+  print ("starting task " .. ctx.name)
+  
+  local res,msg = schd.subscribe_event_by_name(ctx, "event_1")
+  if res == false then
+    print(msg)
+  end
+  
   local y = 1
   while(1) do
     print("task2, y = " .. y)
@@ -72,8 +175,8 @@ function task2( t )
   end
 end
 
-schd.create_task(task1)
-schd.create_task(task2)
+schd.create_task(task1, "task1")
+schd.create_task(task2, "task2")
 schd.scheduler()
 
 
