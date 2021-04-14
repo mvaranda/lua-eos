332daa19503503b5d5fe7aea18ea482574f74c6a
Marcelo Varanda <mv_email@yahoo.com>
2021-04-14 14:32:04 -0400

    Test app with 2 buttons incrementing via delay loop.

--- lua_eos/native/splash_logo/spiffs_image/app.lua
+++ lua_eos/native/splash_logo/spiffs_image/app.lua
@@ -27,13 +27,23 @@ local btn_1
 local label_1
 local btn_1_cnt = 1
 
+local btn_2
+local label_2
+local btn_2_cnt = 1
+
 local function init_ui()
   print("init_ui: in")
   btn_1 = lv_btn_create(lv_scr_act(), 0)
-  lv_obj_set_pos(btn_1, 10, 10)
+  lv_obj_set_pos(btn_1, 30, 10)
   lv_obj_set_size(btn_1, 120, 50)
   label_1 = lv_label_create(btn_1, 0)
-  lv_label_set_text(label_1, "Button")
+  lv_label_set_text(label_1, "Button_A")
+  
+  btn_2 = lv_btn_create(lv_scr_act(), 0)
+  lv_obj_set_pos(btn_2, 170, 10)
+  lv_obj_set_size(btn_2, 120, 50)
+  label_2 = lv_label_create(btn_2, 0)
+  lv_label_set_text(label_2, "Button_B")
   print("init_ui: out")
 end
 
@@ -58,6 +68,12 @@ function app( ctx )
     end
     --eos.post(ev, "message from task 1")
     eos.delay(ctx, 1000)
-    
+    btn_1_cnt = btn_1_cnt + 1
+    lv_label_set_text(label_1, "Button " .. btn_1_cnt)
+    if (btn_1_cnt % 2) > 0 then
+      btn_2_cnt = btn_2_cnt + 1
+      lv_label_set_text(label_2, "Button " .. btn_2_cnt)
+    end
+      
   end
 end
