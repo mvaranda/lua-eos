2197efa0f1f88492ac4497b29355168c48436856
Marcelo Varanda <mv_email@yahoo.com>
2021-05-05 18:02:08 -0400

    WIP LVGL bind gen.

--- lvgl_patch/lv_mk_bindings.py
+++ lvgl_patch/lv_mk_bindings.py
@@ -15,6 +15,97 @@
  ###############################################################
  #
 
+C_HEADER1 = """
+#ifndef _BIND_$BASENAME$_H_
+#define _BIND_$BASENAME$_H_
+"""
+C_HEADER2 = """
+ /*
+ ###############################################################
+ #
+ #                 This code is part of LUA_EOS
+ #
+ # Copyrights 2021 - Varanda Labs Inc.
+ #
+ # License:
+ #   Creative Commons: CC BY-NC-SA 4.0
+ #   Attribution-NonCommercial-ShareAlike 4.0 International
+ #   https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ #
+ #   Note: for purchasing a commertial license contact:
+ #     m@varanda.ca
+ #
+ ###############################################################
+*/
+
+"""
+L_HEADER = """
+-- ***************************************************************
+-- *
+-- *                 This code is part of LUA_EOS
+-- *
+-- * Copyrights 2021 - Varanda Labs Inc.
+-- *
+-- * License:
+-- *   Creative Commons: CC BY-NC-SA 4.0
+-- *   Attribution-NonCommercial-ShareAlike 4.0 International
+-- *   https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+-- *
+-- *   Note: for purchasing a commertial license contact:
+-- *     m@varanda.ca
+-- *
+-- ***************************************************************
+
+"""
+
+FUNC_DEF = """
+static int $FUNC_NAME$(lua_State *L)
+"""
+
+PAR_OBJ = """
+  void * $PAR_N$ = lua_touserdata(L,$PAR_N$);
+"""
+
+PAR_INT = """
+  int $PAR_N$ = lua_tointeger(L,$PAR_N$);
+"""
+
+PAR_STRING = """
+  const char * $PAR_N$ = lua_tostring(L,$PAR_N$);
+"""
+
+RET_VOID = """
+  return 0;
+}
+"""
+
+RET_OBJ = """
+  if (! ret) {
+    lua_pushnil(L);
+  }
+  else {
+    lua_pushlightuserdata(L, ret);
+  }
+  return 1;
+}
+"""
+
+RET_INT = """
+  lua_pushinteger(L, ret)
+  return 1
+}
+"""
+
+RET_INT = """
+  lua_pushstrinf(L, ret)
+  return 1
+}
+"""
+
+BIND_LIST_DEF = """
+void $BASENAME$_init(lua_State *L)
+{
+"""
 import sys, os
 
 VERSION = "0.01"
@@ -28,6 +119,8 @@ root_dirs = {
 exclude_dirs = {
   "../lvgl/src/lv_gpu"
 }
+  
+ENDL = "\n"
 
 def create_file_list():
   files = []
@@ -90,13 +183,10 @@ def parse_function(line):
   ret = None
   dic = {}
 
-#a = {"name" : "myfunc", "num_params": 5, "params" : [ {"type" : "char", "varname" : "buf"} ] }
-#print(a)
-#{'name': 'myfunc', 'num_params': 5, 'params': [{'type': 'char', 'varname': 'buf'}]}
-#b = {"name" : "myfuncB", "num_params": 2, "params" : [ {"type" : "char", "varname" : "buf"} ] }
-#f = [a,b]
-#print(f[1])
-#{'name': 'myfuncB', 'num_params': 2, 'params': [{'type': 'char', 'varname': 'buf'}]}
+#example:
+# void lv_obj_init_draw_line_dsc(lv_obj_t * obj, uint8_t part, lv_draw_line_dsc_t * draw_dsc);
+# {'name': 'lv_obj_init_draw_line_dsc', 'ret': 'void', 'num_params': 3, 'params': [{'type': 'lv_obj_t *', 'name': 'obj'}, {'type': 'uint8_t', 'name': 'part'}, {'type': 'lv_draw_line_dsc_t *', 'name': 'draw_dsc'}]}
+
   try:
     l_r = line.split("(")
     l = l_r[0].split()
@@ -122,7 +212,6 @@ def parse_function(line):
 
   except:
     return "\n\nFail to parse: " + line + "\n\n", None
-  print(dic)
   return None, dic
 
 
@@ -149,10 +238,15 @@ def parse_obj_functions(filename):
     if l.find(";") >= 0:
       [err, f_dic] = parse_function(l)
       if err == None:
-        funcs.append(f_dic)
+        funcs.append([f_dic, l])
       else:
         print(err)
       l = ""
+  return funcs
+
+def append_function(bind_array, c_file, ch_file, f):
+  c_file.write("// " + f[1] + ENDL)
+  ch_file.write("// " + f[1] + ENDL)
   
 
 def mk():
@@ -163,7 +257,26 @@ def mk():
   
   files = [ '../lvgl/src/lv_core/lv_obj.h' ]
   for file in files:
-    parse_obj_functions(file)
+    print("processing file: " + file)
+    base_name = file.split('/')
+    base_name = base_name[len(base_name)-1]
+    base_name = base_name.replace('.h','')
+    print("base_name = " + base_name)
+    funcs = parse_obj_functions(file)
+    #print(funcs)
+    c_file = open("bind_" + base_name + ".c", 'w')
+    c_file.write(C_HEADER2)
+    ch_file = open("bind_" + base_name + ".h", 'w')
+    ch_file.write(C_HEADER1.replace("$BASENAME$", base_name))
+    ch_file.write(C_HEADER2)
+                  
+    bind_array = BIND_LIST_DEF
+    bind_array.replace("$BASENAME$", base_name)
+    for f in funcs:           
+      bind_array = append_function(bind_array, c_file, ch_file, f)
+                  
+    c_file.close()
+    ch_file.close()
   
 if __name__ == "__main__":
   print("lv_mkbindings version %s\n" % VERSION)
